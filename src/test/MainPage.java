bf2c9d4ab5e1b03bb947656defb94e3116c6b859
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-03 20:11:59 +0200

    Added main page loader

---
+++ src/test/MainPage.java
@@ -0,0 +1,23 @@
+import org.junit.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+
+
+class MainPage extends PageBase {
+
+    private By loginButtonLocator = By.xpath("//a[@href='/login']");
+    
+    public MainPage(WebDriver driver) {
+        super(driver);
+        this.driver.get("https://notion.so/");
+    }
+}
