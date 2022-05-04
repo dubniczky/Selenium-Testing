4f012ec84c6a89e4f4b6b42043af7a2e7e617b05
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-05-04 14:58:08 +0200

    Added main test class

---
+++ src/test/java/NotionTest.java
@@ -0,0 +1,40 @@
+import org.junit.*;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import java.util.*;  
+
+
+public class NotionTest {
+    public WebDriver driver;
+    
+    @Before
+    public void setup() {
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void LoginTest() {
+        MainPage mainPage = new MainPage(this.driver);
+
+        Assert.assertTrue( mainPage.getTitle() == "asd" );
+    }
+    
+    @After
+    public void close() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
